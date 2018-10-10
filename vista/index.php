7baa5f6004b9e581ba9514b18ef7be9f7427d530
desarrolloJuan <desarrolloJuan@ASUS-JuanK>
2018-10-10 16:42:27 -0500

    se agrego el chat inicial, cambio en ajax para index loging y registro de noticias

--- vista/index.php
+++ vista/index.php
@@ -1,4 +1,6 @@
-
+<?php
+ include '../modelo/Usuario.php';
+?>
 <html lang="esp">
     <head>
         <meta charset="UTF-8">
@@ -22,21 +24,21 @@
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">-->
         <?php
         session_start();
-        $user = NULL;
+        $user = new Usuario();
         if (isset($_SESSION["user"])) {
-            
+            $user = unserialize($s['usuario']);
         }
         ?>
         <style>
 
-.row img{
-    padding: 0px 10px;
-    width: 100%;
-}
-.text-max-170{
-    width: 90%;
-    text-align: justify;
-}
+            .row img{
+                padding: 0px 10px;
+                width: 100%;
+            }
+            .text-max-170{
+                width: 90%;
+                text-align: justify;
+            }
 
 
 
@@ -247,61 +249,71 @@
                         </div>-->
         </div>
         <div id="loader"></div>
-        <!--inici de chat-->
-        <div id="chat-content">
-            <div class="conversacion">
-                <div class="titulo-chat">
-                </div>
-                <div class="cuerpo-chat scroll-item scroll-blue" id="chat_7">
-                    <div class="mensaje-izq">
-                        <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> </div>
-                        <div class="text-mensaje">
-                            <p class="mensaje">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, </p>
-                            <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
+        <?php
+        
+        if ($user->getId()>0) {
+            
+            ?>
+            <!--inici de chat-->
+            <div id="chat-content">
+                <div class="conversacion">
+                    <div class="titulo-chat">
+                    </div>
+                    <div class="cuerpo-chat scroll-item scroll-blue" id="chat_7">
+                        <div class="mensaje-izq">
+                            <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> </div>
+                            <div class="text-mensaje">
+                                <p class="mensaje">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, </p>
+                                <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
+                            </div>
+                        </div>
+                        <div class="mensaje-der">
+                            <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> </div>
+                            <div class="text-mensaje">
+                                <p class="mensaje">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, </p>
+                                <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
+                            </div>
+                        </div>
+                        <div class="mensaje-izq">
+                            <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> </div>
+                            <div class="text-mensaje">
+                                <p class="mensaje">Lorem Ipsum es. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, </p>
+                                <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
+                            </div>
+                        </div>
+                        <div class="mensaje-izq">
+                            <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> 
+                            </div>
+                            <div class="text-mensaje">
+                                <p class="mensaje">Lorem Ipsum es. desde el año 1500, </p>
+                                <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
+                            </div>
                         </div>
                     </div>
-                    <div class="mensaje-der">
-                        <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> </div>
-                        <div class="text-mensaje">
-                            <p class="mensaje">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, </p>
-                            <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
-                        </div>
+                    <div class="mensaje-chat ">
+                        <form action="../controlador/chats.php" method="post" onsubmit="envioFormulario(this, 'chat_7', false); return false;">
+                            <!--<form action="../controlador/chats.php" method="post" >-->
+                            <input type="hidden" name="oper" value="envio mensaje">
+                            <input type="hidden" name="idRecibe" value="7">
+                            <textarea name="mensaje"  class="form-control input-chat scroll-item scroll-blue"></textarea>
+                            <a href="#1" class="btn btn-sm btn-defauld btn-chat" onclick="$(this).parent().submit();"><i class="fa fa-paper-plane"></i></a>
+                        </form>
                     </div>
-                    <div class="mensaje-izq">
-                        <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> </div>
-                        <div class="text-mensaje">
-                            <p class="mensaje">Lorem Ipsum es. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, </p>
-                            <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
-                        </div>
-                    </div>
-                    <div class="mensaje-izq">
-                        <div class="nomb-cuerpo-chat"> <img class="img-user-chat" src="../../vista/Imagenes/22.png"> 
-                        </div>
-                        <div class="text-mensaje">
-                            <p class="mensaje">Lorem Ipsum es. desde el año 1500, </p>
-                            <span class="f-h-mensaje">24 abr 2017 | 23:45</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="mensaje-chat ">
-                    <form action="../controlador/chats.php" method="post" onsubmit="envioFormulario(this,'chat_7',false); return false;">
-                    <!--<form action="../controlador/chats.php" method="post" >-->
-                        <input type="hidden" name="oper" value="envio mensaje">
-                        <input type="hidden" name="idRecibe" value="7">
-                        <textarea name="mensaje"  class="form-control input-chat scroll-item scroll-blue"></textarea>
-                        <a href="#1" class="btn btn-sm btn-defauld btn-chat" onclick="$(this).parent().submit();"><i class="fa fa-paper-plane"></i></a>
-                    </form>
                 </div>
             </div>
-        </div>
-        <!--final del chat-->
+
+            <!--final del chat-->
+            <?php
+        }
+        ?>
+
         <footer >
             <div class="pie">
                 <p>
                     <a href="index.php">Inicio</a> | 
-                    <a href="#contactenos.php" onclick="cargarPagina('contactenos.php','contenedorPrincipal',true);">Contáctenos</a> |
-                    <a href="#registro.php" onclick="cargarPagina('registro.php','contenedorPrincipal',true);">Registro</a> |
-                    <a href="#ingreso.php" onclick="cargarPagina('ingreso.php','contenedorPrincipal',true);">Login</a> |                                                
+                    <a href="#contactenos.php" onclick="cargarPagina('contactenos.php', 'contenedorPrincipal', true);">Contáctenos</a> |
+                    <a href="#registro.php" onclick="cargarPagina('registro.php', 'contenedorPrincipal', true);">Registro</a> |
+                    <a href="#ingreso.php" onclick="cargarPagina('ingreso.php', 'contenedorPrincipal', true);">Login</a> |                                                
                 </p>
                 <p>
                     Copyright 2016. <a href="http://www.uis.edu.co/" rel="develop">Universidad Industrial de Santander</a>   <a href="http://www.filosofiayensenanza.org/inicio/" rel="develop">Grupo FiloEn</a>
@@ -311,10 +323,10 @@
         <!--<script src="js/jquery.min.js"></script>-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        
-       
+
+
 <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>-->
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>-->
         <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
         <script src="js/funcionesGenerales.js" type="text/javascript"></script>
         <script src="js/jquery.flexslider.js"></script>
@@ -324,11 +336,11 @@
         <script src="pluging/lineControlEditor/editor.js" type="text/javascript"></script>
 
         <script type="text/javascript" charset="utf-8">
-                $(window).load(function () {
-                    $('.flexslider').flexslider();
-                    inicioLoader();
+                        $(window).load(function () {
+                            $('.flexslider').flexslider();
+                            inicioLoader();
 
-                });
+                        });
         </script>
     </body>
 </html>
